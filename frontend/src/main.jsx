753394ef31eb97e4baa95c63854e4f3d02de36a4
Ashishreddy-Pirateking <www.ashishreddy@gmail.com>
2026-04-27 00:45:47 +0530

    Add spotlight styles to React bundle

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./fonts.css";
+import "./index.css";
 import "./Legacy/style.css";
 import "./Legacy/gallery.css";
 import "./ticket-overrides.css";
