b4e2d5ae930b4e4772d383366ea5a048896381a3
Ashishreddy-Pirateking <www.ashishreddy@gmail.com>
2026-04-27 00:55:31 +0530

    Strengthen cursor spotlight halo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./fonts.css";
-import "./index.css";
 import "./Legacy/style.css";
 import "./Legacy/gallery.css";
 import "./ticket-overrides.css";
+import "./index.css";
 import App from "./App.jsx";
 import { SiteContentProvider } from "./context/SiteContentContext.jsx";
 
